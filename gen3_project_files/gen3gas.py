4f10df959c0b7b6993c2a895ebc5c46494744b07
Jack-Hinze <jack.hinze@braytonenergy.com>
2021-03-19 10:39:27 -0400

    Update gen3gas.py

--- gen3_project_files/gen3gas.py
+++ gen3_project_files/gen3gas.py
@@ -976,7 +976,7 @@ class Gen3opt:
 
         #field costs
         ssc.data_set_number( data, b'site_spec_cost', 10. );
-        ssc.data_set_number( data, b'heliostat_spec_cost', 75 );
+        ssc.data_set_number( data, b'heliostat_spec_cost', 50 );
 
         #Plant and BOP
         ssc.data_set_number( data, b'plant_spec_cost', 600 );
